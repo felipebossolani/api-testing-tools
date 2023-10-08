459f229f7b20f042e4e3d0c369b6f962ebad3f90
Felipe Bossolani <fbossolani@gmail.com>
2023-10-07 23:00:43 -0300

    Add: Grouped user endpoints in an isolated class

---
+++ src/api/TasksApi/Endpoints/UserEndpoints.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace TasksApi;
+
+public static class UserEndpoints
+{
+    public static void AddUsersEndpoints(this IEndpointRouteBuilder app)
+    {
+        var users = app.MapGroup("/users");
+        users.MapGet("/", GetAllUsers);
+        users.MapGet("/{id}", GetUser);
+        users.MapPost("/", AddUser);
+        users.MapPut("/{id}", UpdateUser);
+        users.MapDelete("/{id}", DeleteUser);
+    }
+
+    static async Task<IResult> GetAllUsers(ApiContext context) => 
+        TypedResults.Ok(await context.Users.ToListAsync());
+
+    static async Task<IResult> GetUser(ApiContext context, Guid id) => 
+        await context.Users.FindAsync(id) is User user ? Results.Ok(user) : Results.NotFound();
+
+    static async Task<IResult> AddUser(Validated<InsertUserRequest> request, IMapper mapper, ApiContext context) {
+        var (isValid, value) = request;
+
+        if (!isValid) return Results.ValidationProblem(request.Errors);
+
+        var user = mapper.Map<User>(value);
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return Results.Created($"/users/{user.Id}", user);
+    }
+    static async Task<IResult> UpdateUser(Guid id, Validated<UpdateUserRequest> request, ApiContext context) {
+        var (isValid, value) = request;
+        if (!isValid) return Results.ValidationProblem(request.Errors);
+
+        var user = await context.Users.FindAsync(id);
+        if (user is null) return Results.NotFound();
+
+        user.Name = value.Name;
+        await context.SaveChangesAsync();
+
+        return Results.NoContent();
+    }
+    static async Task<IResult> DeleteUser(Guid id, ApiContext context) {
+        if (!(await context.Users.FindAsync(id) is User user))
+            return Results.NotFound();
+
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+    }
+}
--- src/api/TasksApi/Program.cs
+++ src/api/TasksApi/Program.cs
@@ -19,52 +19,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TasksApi v1"));
 
-var users = app.MapGroup("/users");
-
-users.MapGet("/", async (ApiContext context) =>
-{
-    return await context.Users.ToListAsync();
-});
-users.MapGet("/{id}", async (ApiContext context, Guid id) =>
-{
-    return await context.Users.FindAsync(id) is User user ? Results.Ok(user) : Results.NotFound();
-});
-users.MapPost("/", async (Validated<InsertUserRequest> request, IMapper mapper, ApiContext context) =>
-{
-    var (isValid, value) = request;
-
-    if (!isValid) return Results.ValidationProblem(request.Errors);
-
-    var user = mapper.Map<User>(value);
-    context.Users.Add(user);
-    await context.SaveChangesAsync();
-
-    return Results.Created($"/users/{user.Id}", user);
-});
-
-users.MapPut("/{id}", async (Guid id, Validated<UpdateUserRequest> request, ApiContext context) =>
-{
-    var (isValid, value) = request;
-    if (!isValid) return Results.ValidationProblem(request.Errors);
-
-    var user = await context.Users.FindAsync(id);
-    if (user is null) return Results.NotFound();
-
-    user.Name = value.Name;
-    await context.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-users.MapDelete("/{id}", async (Guid id, ApiContext context) =>
-{
-    if (!(await context.Users.FindAsync(id) is User user)) 
-        return Results.NotFound();
-    
-    context.Users.Remove(user);
-    await context.SaveChangesAsync();
-    return Results.NoContent();    
-});
+app.AddUsersEndpoints();
 
 SeedDatabase(app);
 app.Run();
